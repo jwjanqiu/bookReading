354926590b48c9a16b22dad0357a1a311c9c15d3
jwjanqiu <jwjanqiu@outlook.com>
2019-04-08 19:35:16 +0800

    1.0.11 新增修改用户资料

--- pages/mine/settings/settings.js
+++ pages/mine/settings/settings.js
@@ -6,7 +6,132 @@ Page({
    * 页面的初始数据
    */
   data: {
+    onOff: true,
+    userName: '',
+    password: '',
+    confirmPassword: ''
+  },
 
+  /**modal弹框 */
+  btnclick: function() {
+    var onOff = this.data.onOff;
+    this.setData({
+      onOff: !onOff,
+      userName: app.globalData.userName
+    });
+  },
+
+  /**
+   * modal确认
+   */
+  modalConfirm: function(e) {
+    var userName = this.data.userName
+    var password = this.data.password
+    var confirmPassword = this.data.confirmPassword
+    var url = app.globalData.url
+    var that = this
+    //modal状态
+    var onOff = this.data.onOff
+    //密码有输入
+    if (password || confirmPassword) {
+      //密码不对应
+      if (password != confirmPassword) {
+        wx.showToast({
+          title: '两次密码不一致',
+          icon: 'none',
+          duration: 1000
+        })
+        return;
+      }
+    }
+    //无密码和用户名未修改不请求
+    if (!password && (userName == app.globalData.userName)) {
+      that.setData({
+        onOff: !onOff
+      })
+      return;
+    }
+
+    wx.request({
+      url: url + '/modifyUserInfo',
+      data: {
+        token: app.globalData.token,
+        user_name: userName,
+        password: password
+      },
+      method: 'POST',
+      success: function(res) {
+        console.log(res.data)
+        if (res.data.code == 1) {
+          app.globalData.userName = res.data.data.nick_name
+          //关闭modal
+          that.setData({
+            onOff: !onOff,
+            userName: res.data.data.nick_name,
+            password: '',
+            confirmPassword: ''
+          })
+          //修改storage信息
+          var _userInfo = wx.getStorageSync('userInfo')
+          _userInfo.userName = res.data.data.nick_name
+          wx.setStorageSync('userInfo', _userInfo)
+          wx.showToast({
+            title: '修改成功',
+            icon: 'none',
+            duration: 1000
+          })
+          setTimeout(function() {
+            wx.switchTab({
+              url: '../mine',
+            })
+          }, 500)
+        } else {
+          //失败时提示失败原因
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none',
+            duration: 1000
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * modal隐藏 
+   */
+  modalCancel: function(e) {
+    var onOff = this.data.onOff
+    this.setData({
+      onOff: !onOff
+    });
+  },
+
+  /**
+   * 用户名输入
+   */
+  userNameInput: function(e) {
+    this.setData({
+      userName: e.detail.value
+    })
+  },
+
+  /**
+   * 密码输入
+   */
+  passwordInput: function(e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+
+  /**
+   * 确认密码输入
+   */
+  confirmPasswordInput: function(e) {
+    this.setData({
+      confirmPassword: e.detail.value
+    })
   },
 
   /**
@@ -21,6 +146,7 @@ Page({
       },
       method: "POST",
       success: function(res) {
+        //清除缓存
         wx.removeStorage({
           key: 'userInfo',
           success: function(res) {
